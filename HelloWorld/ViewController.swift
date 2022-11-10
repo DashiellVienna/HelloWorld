6fa1b2dbe9afc23614eb6c0a0b396cfc1a645523
Andrei Deshin <andrei.deshin@gmail.com>
2022-11-10 22:35:01 +0100

    work with button configuration

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet var greetingLabel: UILabel!
     @IBOutlet var greetingButton: UIButton!
     
@@ -18,14 +17,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         greetingLabel.isHidden = true
-        greetingButton.layer.cornerRadius = 10
+       // greetingButton.layer.cornerRadius = 10
+        greetingButton.configuration = setupButton(with: "Show Greeting")
+        // размер кнопки через frame
+        //greetingButton.frame(ширина высота)
     }
-    
     
     @IBAction func greetingButtonTapped() {
         //print("Button is tapped")
         greetingLabel.isHidden.toggle()
-        
+        // 1 option
         //        if greetingLabel.isHidden {
         //            greetingButton.setTitle("Show Greeting", for: .normal)
         //        }else {
@@ -33,9 +34,30 @@ class ViewController: UIViewController {
         //        }
         //    }
         
-        greetingButton.setTitle(
-            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
-            for: .normal
+        // 2 option
+//        greetingButton.setTitle(
+//            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+//            for: .normal
+//        )
+        
+        // 3 option
+        greetingButton.configuration = setupButton(
+            with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting"
         )
+        
+    }
+    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        // buttonConfiguration.baseBackgroundColor = UIColor.red     or for custom color #colorLiteral(  !!!!!
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0, green: 0.617862165, blue: 0.8836789131, alpha: 1)
+        buttonConfiguration.title = title
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        // custom font
+        // buttonConfiguration.attributedTitle?.font = .
+        
+        return buttonConfiguration
     }
 }
